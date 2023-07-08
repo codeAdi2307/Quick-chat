742a702da9019ed77801191fda2ecae16b55a07e
Aditya Kumar Lal <138471886+codeAdi2307@users.noreply.github.com>
2023-07-08 03:44:38 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div> Home Page </div>} />
+        <Route path='/about' element={<div> About Page </div>} />
+        <Route path='/contact' element={<div> Contact Page </div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
